be451b90355eba833668542914b05eda9e618cae
jmallari21 <104325926+jmallari21@users.noreply.github.com>
2022-11-17 20:39:10 -0600

    Update backup_chatclient.cpp

--- backup_chatclient.cpp
+++ backup_chatclient.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <bits/stdc++.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -7,7 +8,6 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <thread>
-#include <signal.h>
 #include <mutex>
 #define MAX_LEN 200
 #define NUM_COLORS 6
@@ -15,97 +15,108 @@
 
 using namespace std;
 
-bool exit_flag=false;
-thread t_send, t_recv;
-int client_socket;
-string def_col="\033[0m";
-string colors[]={"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m"};
+struct terminal
+{
+	int id;
+	string name;
+	int socket;
+	thread th;
+	string chatroom;
+};
 
-void catch_ctrl_c(int signal);
+
+vector<terminal> clients;
+vector<string> chatrooms;
+
+
+string def_col="\033[0m";
+string colors[]={"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m","\033[36m"};
+int seed=0;
+mutex cout_mtx,clients_mtx;
+
 string color(int code);
-int eraseText(int cnt);
-void send_message(int client_socket);
-void recv_message(int client_socket);
+void set_name(int id, char name[]);
+void shared_print(string str, bool endLine);
+int broadcast_message(string message, int sender_id);
+int broadcast_message(int num, int sender_id);
+void end_connection(int id);
+void handle_client(int client_socket, int id);
 
 int main(int argc, char const* argv[]) {
 	
-	if(argc != 2) // if argc (length of argv) != 3, error & quit
+	if(argc != 2) // if argc (length of argv) != 2, error & quit
 	{
 		cout << endl << "ERROR: Invalid or Missing Port" << endl;
 		cout << endl << "------ Specify port or type DEFAULT for default port " << PORTNUM << endl << endl;
 		cout << endl << "Usage: " << argv[0] << " <port>" << endl << endl;
 		return -1;
 	}
+
+	//debug print
+	cout << "Port is : " << argv[1] << endl << endl;
 	
-	//debug prints
-	//cout << "IP Address is : " << argv[1] << endl;
-	//cout << "Port is : " << argv[1] << endl << endl;
 
-	cout <<" \n\n\t  ====== Welcome to the Chatroom Lobby ======   \n" << endl;
-
-	if((client_socket=socket(AF_INET,SOCK_STREAM,0))==-1)
+	int server_socket;
+	if((server_socket=socket(AF_INET,SOCK_STREAM,0))==-1)
 	{
 		perror("socket: ");
 		exit(-1);
 	}
 
-	struct sockaddr_in client;
-	client.sin_family=AF_INET;
+	struct sockaddr_in server;
+	server.sin_family=AF_INET;
+
+	cout << "Port is : " << server.sin_port << endl << endl;
 
 	if(argc = 2 && strcmp(argv[1], "DEFAULT") != 0 )
-		client.sin_port=htons((unsigned)atoi(argv[1])); // Port no. of server
+		server.sin_port=htons((u_short)atoi(argv[1])); // set port no. of server
 	else{
-		cout << "\n\t  === note: using default port "<< PORTNUM <<" ===" << endl << endl;
-		client.sin_port=htons(PORTNUM); // Port no. of server
+		cout << "\n\t  === note: using default port "<< PORTNUM <<" ===" << endl;
+		server.sin_port=htons(PORTNUM); // set port no. of server
 	}
 
-	client.sin_addr.s_addr=INADDR_ANY;
-	//client.sin_addr.s_addr=inet_addr("127.0.0.1"); // Provide IP address of server
-	bzero(&client.sin_zero,0);
+	server.sin_addr.s_addr=INADDR_ANY;
+	bzero(&server.sin_zero,0);
 
-	if((connect(client_socket,(struct sockaddr *)&client,sizeof(struct sockaddr_in)))==-1)
+	if((bind(server_socket,(struct sockaddr *)&server,sizeof(struct sockaddr_in)))==-1)
 	{
-		perror("connect: ");
+		perror("bind error: ");
 		exit(-1);
 	}
-	signal(SIGINT, catch_ctrl_c);
-	char name[MAX_LEN];
-	cout<<"Enter your name : ";
-	cin.getline(name,MAX_LEN);
-	send(client_socket,name,sizeof(name),0);
 
-	char chatroom[MAX_LEN];
-	cout<< endl << "   ==== Enter a chatroom name if it exists. If not, it will be created. ====" << endl;
-	cout<<"\nChatroom name : ";
-	cin.getline(chatroom,MAX_LEN);
-	send(client_socket,chatroom,sizeof(chatroom),0);
+	if((listen(server_socket,8))==-1)
+	{
+		perror("listen error: ");
+		exit(-1);
+	}
 
-	cout<<colors[NUM_COLORS-1]<<"\n\n\t  ====== Welcome to " << chatroom << " ======   \n"<<endl<<def_col;
+	struct sockaddr_in client;
+	int client_socket;
+	unsigned int len=sizeof(sockaddr_in);
 
-	thread sendThread(send_message, client_socket);
-	thread recvThread(recv_message, client_socket);
+	cout<<colors[NUM_COLORS-1]<<"\n\t  ====== Chatroom Server Log Started ======   \n"<<endl<<def_col;
 
-	t_send=move(sendThread);
-	t_recv=move(recvThread);
+	while(1)
+	{
+		if((client_socket=accept(server_socket,(struct sockaddr *)&client,&len))==-1)
+		{
+			perror("accept error: ");
+			exit(-1);
+		}
+		seed++;
+		thread t(handle_client,client_socket,seed);
+		lock_guard<mutex> guard(clients_mtx);
+		clients.push_back({seed, string("Anonymous"),client_socket,(move(t))});
+	}
 
-	if(t_send.joinable())
-		t_send.join();
-	if(t_recv.joinable())
-		t_recv.join();
-			
+	for(int i=0; i<clients.size(); i++)
+	{
+		if(clients[i].th.joinable())
+			clients[i].th.join();
+	}
+
+	close(server_socket);
 	return 0;
-}
-
-// Handler for "Ctrl + C"
-void catch_ctrl_c(int signal) 
-{
-	char str[MAX_LEN]="#exit";
-	send(client_socket,str,sizeof(str),0);
-	exit_flag=true;
-	t_send.detach();
-	t_recv.detach();
-	close(client_socket);
-	exit(signal);
 }
 
 string color(int code)
@@ -113,56 +124,152 @@ string color(int code)
 	return colors[code%NUM_COLORS];
 }
 
-// Erase text from terminal
-int eraseText(int cnt)
+// Set name of client
+void set_name(int id, char name[])
 {
-	char back_space=8;
-	for(int i=0; i<cnt; i++)
+	for(int i=0; i<clients.size(); i++)
 	{
-		cout<<back_space;
-	}
+			if(clients[i].id==id)	
+			{
+				clients[i].name=string(name);
+			}
+	}	
+}
+
+void set_room(int id, char room[])
+{
+	for(int i=0; i<clients.size(); i++)
+	{
+			if(clients[i].id==id)	
+			{
+				clients[i].chatroom=string(room);
+				for(int j = 0; j < chatrooms.size(); i++)
+					if(chatrooms.size() > 0 && clients[i].chatroom.compare(chatrooms[i]) == 0)
+					{
+						cout << "Room already exists" << endl;
+					}
+					else
+					{
+						chatrooms.push_back(clients[i].chatroom); 
+						cout << "Room created" << endl;
+					}
+			}
+	}	
+}
+
+// For synchronisation of cout statements
+void shared_print(string str, bool endLine = true)
+{	
+	lock_guard<mutex> guard(cout_mtx);
+	cout<<str;
+	if(endLine)
+			cout<<endl;
+}
+
+// Broadcast message to all clients except the sender
+int broadcast_message(string message, int sender_id)
+{
+	char temp[MAX_LEN];
+	strcpy(temp,message.c_str());
+	for(int i=0; i<clients.size(); i++)
+	{
+		if(clients[i].id!=sender_id)
+		{
+			send(clients[i].socket,temp,sizeof(temp),0);
+		}
+	}		
 	return 0;	
 }
 
-// Send message to everyone
-void send_message(int client_socket)
+int multicast_message(string message, int sender_id, string sender_rm)
 {
-	while(1)
+	char temp[MAX_LEN];
+	strcpy(temp,message.c_str());
+	for(int i=0; i<clients.size(); i++)
 	{
-		cout<<colors[1]<<"You : "<<def_col;
-		char str[MAX_LEN];
-		cin.getline(str,MAX_LEN);
-		send(client_socket,str,sizeof(str),0);
-		if(strcmp(str,"#exit")==0)
+		if(clients[i].id!=sender_id && clients[i].chatroom.compare(sender_rm) == 0)
 		{
-			exit_flag=true;
-			t_recv.detach();	
-			close(client_socket);
-			return;
-		}	
-	}		
+			send(clients[i].socket,temp,sizeof(temp),0);
+		}
+	}
+	return 0;		
 }
 
-// Receive message
-void recv_message(int client_socket)
+int multicast_message(int num, int sender_id, string sender_rm)
 {
+	for(int i=0; i<clients.size(); i++)
+	{
+		if(clients[i].id!=sender_id && clients[i].chatroom.compare(sender_rm) == 0)
+		{
+			send(clients[i].socket,&num,sizeof(num),0);
+		}
+	}	
+	return 0;		
+}
+
+// Broadcast a number to all clients except the sender
+int broadcast_message(int num, int sender_id)
+{
+	for(int i=0; i<clients.size(); i++)
+	{
+		if(clients[i].id!=sender_id)
+		{
+			send(clients[i].socket,&num,sizeof(num),0);
+		}
+	}	
+	return 0;		
+}
+
+void end_connection(int id)
+{
+	for(int i=0; i<clients.size(); i++)
+	{
+		if(clients[i].id==id)	
+		{
+			lock_guard<mutex> guard(clients_mtx);
+			clients[i].th.detach();
+			clients.erase(clients.begin()+i);
+			close(clients[i].socket);
+			break;
+		}
+	}				
+}
+
+void handle_client(int client_socket, int id)
+{
+	char name[MAX_LEN],str[MAX_LEN], chatroom[MAX_LEN];
+	recv(client_socket,name,sizeof(name),0);
+	set_name(id,name);	
+
+	recv(client_socket,chatroom,sizeof(chatroom),0);
+	set_room(id,chatroom);
+
+	// Display welcome message
+	string welcome_message=string(name)+string(" has joined ")+string(chatroom);
+	multicast_message("#NULL",id,chatroom);	
+	multicast_message(id,id,chatroom);								
+	multicast_message(welcome_message,id,chatroom);	
+	shared_print(color(id)+welcome_message+def_col);
+	
 	while(1)
 	{
-		if(exit_flag)
-			return;
-		char name[MAX_LEN], str[MAX_LEN];
-		int color_code;
-		int bytes_received=recv(client_socket,name,sizeof(name),0);
+		int bytes_received=recv(client_socket,str,sizeof(str),0);
 		if(bytes_received<=0)
-			continue;
-		recv(client_socket,&color_code,sizeof(color_code),0);
-		recv(client_socket,str,sizeof(str),0);
-		eraseText(6);
-		if(strcmp(name,"#NULL")!=0)
-			cout<<color(color_code)<<name<<" : "<<def_col<<str<<endl;
-		else
-			cout<<color(color_code)<<str<<endl;
-		cout<<colors[1]<<"You : "<<def_col;
-		fflush(stdout);
+			return;
+		if(strcmp(str,"#exit")==0)
+		{
+			// Display leaving message
+			string message=string(name)+string(" has left");		
+			multicast_message("#NULL",id,chatroom);			
+			multicast_message(id,id,chatroom);						
+			multicast_message(message,id,chatroom);
+			shared_print(color(id)+message+def_col);
+			end_connection(id);							
+			return;
+		}
+		multicast_message(string(name),id,chatroom);					
+		multicast_message(id,id,chatroom);		
+		multicast_message(string(str),id,chatroom);
+		shared_print(color(id)+name+" : "+def_col+str);		
 	}	
 }
